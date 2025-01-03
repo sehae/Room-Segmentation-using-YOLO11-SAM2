666e7c167866083252797660a4a68632173aae99
sensehae <leahvillatura@myyahoo.com>
2025-01-04 00:58:04 +0800

    Training YOLO11

--- Test.py
+++ Test.py
@@ -1,5 +1,17 @@
 from ultralytics import YOLO
 from ultralytics import SAM
 
-# Load the YOLO modell
-yolo_model = YOLO()
+# Load the YOLO model
+yolo_model = YOLO("")
+
+# Run batched inference on a list of images
+results = yolo_model("")
+
+# Load the SAM model
+sam_model = SAM("")
+
+for result in results:
+    class_ids = result.boxes.cls.int().tolist()
+    if len(class_ids):
+        boxes = result.boxes.xyxy # Boxes object for bbox outputs
+        sam_results = sam_model(result.orig_img, bboxes=boxes, verbose=False, save=True, device=0)
