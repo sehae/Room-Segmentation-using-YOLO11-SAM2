1998d6b13390adf6efb00fe1adb5515b46dfce33
sensehae <leahvillatura@myyahoo.com>
2025-01-04 11:55:47 +0800

    Working Room Detection/Segmentation Prototype
    Prototype is able to decently detect rooms using YOLO11 and SAM2.

--- Test.py
+++ Test.py
@@ -2,16 +2,16 @@ from ultralytics import YOLO
 from ultralytics import SAM
 
 # Load the YOLO model
-yolo_model = YOLO("")
+yolo_model = YOLO("Trained YOLO11 Model/best.pt")
 
 # Run batched inference on a list of images
-results = yolo_model("")
+results = yolo_model("Data/sample3.jpg")
 
 # Load the SAM model
-sam_model = SAM("")
+sam_model = SAM("sam2_b.pt")
 
 for result in results:
     class_ids = result.boxes.cls.int().tolist()
     if len(class_ids):
         boxes = result.boxes.xyxy # Boxes object for bbox outputs
-        sam_results = sam_model(result.orig_img, bboxes=boxes, verbose=False, save=True, device=0)
+        sam_results = sam_model(result.orig_img, bboxes=boxes, verbose=False, save=True, device="cpu")
